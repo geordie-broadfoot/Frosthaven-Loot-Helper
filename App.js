41ab52bc43117a78791d909182ec433d4c010983
geordie-broadfoot <149107205+geordie-broadfoot@users.noreply.github.com>
2024-01-02 14:42:10 -0600

    Initial setup

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar"
+import { StyleSheet, Text, View } from "react-native"
+import * as stuff from "./consts/cards"
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Text>Open up App.js to start working on your apps! </Text>
+			{Object.entries(stuff.nameMaps).map(([k, v]) => (
+				<Text>
+					{k} {" -> "}
+					{v}
+				</Text>
+			))}
+			<StatusBar style="auto" />
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+})
---
+++ consts/cards.js
@@ -0,0 +1,18 @@
+// List of all loot cards
+
+module.exports = {
+	nameMaps: {
+		gold: "Gold",
+		metal: "Metal",
+		logs: "Logs",
+		furs: "Furs",
+		herbs: "Herbs",
+	},
+	cards: {
+		gold: [],
+		metal: [],
+		logs: [],
+		furs: [],
+		herbs: [],
+	},
+}
